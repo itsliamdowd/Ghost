97d945bcde2bcde119a4b42472fbaf96f01826f0
Liam <101684827+SkiingIsFun123@users.noreply.github.com>
2022-09-25 13:38:52 -0700

    Create encode.py

---
+++ encode.py
@@ -0,0 +1,36 @@
+import sys
+import os
+from PIL import Image
+
+def encode():
+    image_name = input("Enter the name of the image: ")
+    image = Image.open(image_name)
+    data = input("Enter the data to encode: ")
+    if len(data) > image.size[0] * image.size[1]:
+        print("The data is too large to fit in the image")
+        sys.exit()
+    data = ''.join(format(ord(i), '08b') for i in data)
+    pixels = image.load()
+    width = image.size[0]
+    height = image.size[1]
+    data_length = len(data)
+    index = 0
+    for y in range(height):
+        for x in range(width):
+            pixel = pixels[x, y]
+            red = pixel[0]
+            green = pixel[1]
+            blue = pixel[2]
+            if index < data_length:
+                red = int(bin(red)[0:7] + data[index], 2)
+                index += 1
+            if index < data_length:
+                green = int(bin(green)[0:7] + data[index], 2)
+                index += 1
+            if index < data_length:
+                blue = int(bin(blue)[0:7] + data[index], 2)
+                index += 1
+            pixels[x, y] = (red, green, blue)
+    image.save("encoded.png")
+
+encode()
